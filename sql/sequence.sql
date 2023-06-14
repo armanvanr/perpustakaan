50535227fa2f3246bc8e820d1ef28b3d66dbdbf3
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-06-14 15:37:27 +0700

    set value of genre sequence

--- sql/sequence.sql
+++ sql/sequence.sql
@@ -6,15 +6,18 @@ CREATE SEQUENCE author_id_seq AS INT MINVALUE 1 MAXVALUE 999 START 1 NO CYCLE;
 CREATE SEQUENCE genre_id_seq AS INT MINVALUE 1 MAXVALUE 999 START 1 NO CYCLE;
 
 CREATE SEQUENCE user_id_seq AS INT MINVALUE 1 MAXVALUE 999 START 1 NO CYCLE;
--- setval of the currentval = 1
-SELECT setval('user_id_seq', 1)
--- setval of the nextval = 1
-SELECT setval('user_id_seq', 1, false)
+-- -- setval of the currentval = 1
+-- SELECT setval('user_id_seq', 1)
+-- -- setval of the nextval = 1
+-- SELECT setval('user_id_seq', 1, false)
+-- SELECT setval('genre_id_seq', 1, false)
 
 CREATE SEQUENCE borrow_id_seq AS INT MINVALUE 1 MAXVALUE 999 START 1 NO CYCLE;
 
 -- TABLES
 SELECT * FROM public.user
+SELECT * FROM public.genre
 
-DELETE FROM public.user
-WHERE id = 'user002'
+
+-- DELETE FROM public.genre
+-- WHERE id = 'bk003'
